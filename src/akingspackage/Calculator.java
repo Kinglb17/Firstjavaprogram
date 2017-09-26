5567a20ec6d2b55e1fdc13c875f88bcd1be83332
Javaclass <Javaclass@15000510cpu.fec.org>
2017-09-26 15:44:06 -0500

    fixed calculator prog

--- src/akingspackage/Calculator.java
+++ src/akingspackage/Calculator.java
@@ -4,7 +4,8 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		System.out.println(calculateSum(6,13));
-printSum(11, 7);
+		System.out.println(calculateSum(14, 55  ));
+
 	}
 static int calculateSum(int a, int b) {
 return a+b;
