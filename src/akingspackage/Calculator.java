557cc7477b5d0dd39d48fbc33cc361b51ffe4951
Javaclass <Javaclass@15000510cpu.fec.org>
2017-09-26 15:26:33 -0500

    Calculate sum of 2 number

---
+++ src/akingspackage/Calculator.java
@@ -0,0 +1,13 @@
+package akingspackage;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		System.out.println(calculateSum(6,13));
+printSum(11, 7);
+	}
+static int calculateSum(int a, int b) {
+return a+b;
+
+}
+}
