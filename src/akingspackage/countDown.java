a62e99c195df694aa413811261294ad8d02b05b9
classroom <classroom@15000510cpu.fec.org>
2017-09-27 15:26:07 -0500

    This was a practice of reverse loops using integers.

---
+++ src/akingspackage/countDown.java
@@ -0,0 +1,16 @@
+package akingspackage;
+
+public class countDown {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num= 10;
+		while (num >=1) {
+			System.out.println(num);
+			num --;
+
+		}
+		
+}
+}
--- src/akingspackage/fruitLoops.java
+++ src/akingspackage/fruitLoops.java
@@ -5,11 +5,11 @@ public class fruitLoops {
 	public static void main(String[] args) {
 		
 	
-	int number = 1;
-		while(number <=10) {
-			System.out.println("LB");
+	int number = 10;
+		while(number <=0) {
+			System.out.println(number);
 		
-			number = number +1;
+			
 			
 		
 
