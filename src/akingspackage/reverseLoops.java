d18ca8f502693b4d30784521571ced0ca48a225d
javaclass <javaclass@15000510cpu.fec.org>
2017-09-28 13:24:03 -0500

    forLoops 20 to 40 with a statement.

---
+++ src/akingspackage/reverseLoops.java
@@ -0,0 +1,16 @@
+package akingspackage;
+
+public class reverseLoops {
+
+	public static void main(String[] args) {
+	 
+	
+	
+//This is a test to test reverse for loops
+		
+		for (int  x= 20; x<= 40; x ++ ) {
+			System.out.println("work Hard Everyday" + " " + x  );
+		}
+		
+	}
+}	
