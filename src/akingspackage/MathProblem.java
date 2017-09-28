cc6b71fc312ad3280d6c5061eb5a13c322ed184e
javaclass <javaclass@15000510cpu.fec.org>
2017-09-28 14:38:18 -0500

    basicmath

---
+++ src/akingspackage/MathProblem.java
@@ -0,0 +1,21 @@
+package akingspackage;
+
+public class MathProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 6;
+		++x;
+		x--;
+		System.out.println(x++);
+		System.out.println(x);
+		System.out.println(++x);
+		System.out.println(x--);
+		System.out.println(--x);
+		int y = 6;
+		y = x - 2;
+		System.out.println("Y is " + y);
+	}
+
+}
+ 
