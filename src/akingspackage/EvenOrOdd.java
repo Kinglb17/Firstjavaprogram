44bb02972821db262ac372cc958a1855f601fb12
javaclass <javaclass@15000510cpu.fec.org>
2017-09-28 15:32:12 -0500

    Only need 2 lines of code to return answer. Lesson on even and odds.

---
+++ src/akingspackage/EvenOrOdd.java
@@ -0,0 +1,19 @@
+package akingspackage;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		evenOrOdd(7);// come with a number that is divide to by 2 to determine odd or even
+			
+	}		
+
+	static void evenOrOdd(int l) {
+		
+		if(l%2 == 0) {
+	System.out.println("Number is even");
+		}
+		else {
+			 System.out.println("Number is odd");
+		}
+	}	
+}
