b595154aedbbcec8e38f01d35ad73c3b8964c470
javaclass <javaclass@15000510cpu.fec.org>
2017-09-28 15:52:59 -0500

    workout plan to rest every 4th and 6th day. Needs corrections

---
+++ src/akingspackage/BreakingPoint.java
@@ -0,0 +1,25 @@
+package akingspackage;
+
+public class BreakingPoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//This code is to implement a workout plan
+		//This list workout days and rest days
+		//This loops until it get to 4th and 6th day
+		for (int  x= 1; x<= 30; x++ ) {
+			if(x == 4 || x == 6 ) {
+			System.out.println("rest day" +" "+ x);
+			
+			}
+			else {
+			System.out.println("work Hard Everyday" + " " + x  );
+		}
+		
+	}
+	
+}
+}
+	
+
+
