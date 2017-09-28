c497fecd3b93fe642ef29f8c68f508df25797c07
javaclass <javaclass@15000510cpu.fec.org>
2017-09-28 12:00:10 -0500

    This was a project to text ForLoops by counting down to 10.

---
+++ src/akingspackage/ForLoops.java
@@ -0,0 +1,16 @@
+package akingspackage;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		// initialize variable, check condition, increment value
+		// Set ForLoops to run command until 10 is reached
+		for(int t=10; t>=0; t--) {
+			//Should print out "Im still hungry plus t
+			System.out.println("Im still hungry "+  t);
+		}
+	
+	}
+	}	
+
+
